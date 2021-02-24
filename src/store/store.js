f7348313ca694d9434d3d57983b52bbad7750567
Marcio Willian <willian_mw.cc@hotmail.com>
2021-02-24 04:20:14 -0300

    Innitial commit

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import carrinho from './modules/carrinho'
+
+import parametros from './modules/parametros'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    modules: {
+        carrinho, parametros
+    }
+})
